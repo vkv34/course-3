cec5b515e501f219508b1d0e60e15306808730b7
vkv34 <d.d08042004@gmail.com>
2024-02-24 21:46:57 +0300

    init

---
+++ SharedCompose/build.gradle.kts
@@ -0,0 +1,93 @@
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+import org.jetbrains.compose.ExperimentalComposeLibrary
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsCompose)
+//    alias(libs.plugins.buildConfig)
+    alias(libs.plugins.kotlinx.serialization)
+}
+
+kotlin {
+    @OptIn(ExperimentalWasmDsl::class)
+    js(IR) {
+        browser()
+    }
+
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    jvm("desktop")
+
+    sourceSets {
+
+        val desktopMain by getting
+
+//         androidMain.dependencies {
+////             implementation(libs.compose.ui.tooling.preview)
+//             implementation(libs.androidx.activity.compose)
+//             implementation(libs.kotlinx.coroutines.android)
+//         }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            implementation(compose.material3)
+            implementation(compose.materialIconsExtended)
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.components.resources)
+            implementation(projects.shared)
+//            implementation(libs.voyager.navigator)
+            implementation(libs.composeImageLoader)
+            implementation(libs.napier)
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.kotlinx.datetime)
+            
+            implementation(projects.domain)
+        }
+
+
+//        jsMain.dependencies {
+//            implementation(compose.html.core)
+//        }
+
+//        iosMain.dependencies {
+//            implementation(libs.ktor.client.darwin)
+//        }
+
+        desktopMain.dependencies {
+            implementation(compose.desktop.currentOs)
+            implementation(compose.preview)
+        }
+//
+//
+//        jvmMain.dependencies {
+//            implementation(compose.desktop.common)
+//            implementation(compose.desktop.currentOs)
+//        }
+    }
+}
+
+android {
+    namespace = "ru.online.education.CourseCompose"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
+dependencies {
+//    implementation(libs.androidx.ui.tooling.preview.android)
+}
